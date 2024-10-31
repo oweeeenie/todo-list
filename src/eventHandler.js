31613b3af9171b270ee4c0c6036b67c2b950fb97
oweeeenie <owensullivan419@yahoo.com>
2024-10-30 20:54:04 -0400

    finished the core functionality of create a project, debugging why it isnt adding though

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,10 +1,15 @@
 import { addTask, getTasks } from './taskManager.js';
 import UIController from './uiController.js';
+import { addProject, getProjects } from './taskManager.js';
 
 const addTaskButton = document.querySelector('.add-task');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('#close-popup');
 const saveTaskButton = document.querySelector('#save-task');
+const addProjectButton = document.querySelector('.create-project');
+const projectPopup = document.querySelector('.popup-project');
+const closeProjectPopupButton = document.querySelector('#close-project-popup');
+const saveProjectButton = document.querySelector('#save-project');
 
 export function setupEventListeners() {
   addTaskButton.addEventListener('click', () => {
@@ -31,5 +36,25 @@ export function setupEventListeners() {
     document.querySelector('#task-desc').value = '';
     const tasks = getTasks();
     UIController.renderTaskList(tasks);
+  });
+
+  addProjectButton.addEventListener('click', () => {
+    projectPopup.classList.remove('hidden');
+  });
+
+  closeProjectPopupButton.addEventListener('click', () => {
+    projectPopup.classList.add('hidden');
+  });
+
+  saveProjectButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const projectName = document.querySelector('#project-name').value;
+
+    addProject(projectName);
+    projectPopup.classList.add('hidden');
+    document.querySelector('#project-name').value = '';
+
+    const projects = getProjects();
+    UIController.renderProjectList(projects);
   });
 }
--- src/taskManager.js
+++ src/taskManager.js
@@ -13,3 +13,19 @@ const addTask = (name, description, date, priority) => {
 
 const getTasks = () => tasks;
 export { addTask, getTasks };
+
+const projects = [];
+
+export function addProject(name) {
+  if (name) {
+    projects.push({ name });
+    console.log(`Project added: ${name}`);
+  } else {
+    console.error('Project name is required.');
+  }
+}
+
+// THIS GETS ALL PROJECTS.
+export function getProjects() {
+  return projects;
+}
